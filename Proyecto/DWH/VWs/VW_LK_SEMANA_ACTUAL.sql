1f47cd89bfd367f6c0826457186ba657e6057099
lspiri <luciano.silvera@gmail.com>
2022-07-19 13:16:37 -0300

    Update VW_LK_SEMANA_ACTUAL.sql
    Quita la Funcion ETL por Permisos

--- Proyecto/DWH/VWs/VW_LK_SEMANA_ACTUAL.sql
+++ Proyecto/DWH/VWs/VW_LK_SEMANA_ACTUAL.sql
@@ -8,5 +8,5 @@ go
 create view VW_LK_SEMANA_ACTUAL AS
 select id_Semana, id_semana_ant 
 from DTPO_LK_SEMANAS 
-where etl.fn_truncFecha ( getdate () ) between semana_Desde and Semana_Hasta
+where DATEADD(dd, 0, DATEDIFF(dd, 0, getdate ()))  between semana_Desde and Semana_Hasta
 go
